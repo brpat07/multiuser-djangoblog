ade29930edde99743ce5d57062e2ac097dcc7620
bittu072 <bittu.patel072@gmail.com>
2017-08-12 22:44:18 -0700

    started new app for blogs and made changes in models

---
+++ src/blogs/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    # auto_now=True, auto_now_add=False means everytime it get updated or edited it saves the time
+    postupdate = models.DateTimeField(auto_now=True, auto_now_add=False)
+    # auto_now=False, auto_now_add=True means it only saves time when it was added to the database
+    posttime = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    # For python2
+    def __unicode__(self):
+        return self.title
+
+    # For python3 
+    def __str__(self):
+        return self.title
