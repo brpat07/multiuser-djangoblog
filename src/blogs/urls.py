6c3314b0fe47e2ae4c7c25ef17f165901cd3902d
bittu072 <bittu.patel072@gmail.com>
2017-08-13 02:12:02 -0700

    added the route for all the crud functionalities in the urls.py of the app

--- src/blogs/urls.py
+++ src/blogs/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
-from .views import blog_read
+from .views import blog_create, blog_read, blog_list, blog_update, blog_delete
 
 urlpatterns = [
-    url(r'^$', blog_read.as_view()),
+    url(r'^$', blog_list.as_view()),
+    url(r'^list$', blog_list.as_view()),
+    url(r'^create$', blog_create.as_view()),
+    url(r'^read$', blog_read.as_view()),
+    url(r'^update$', blog_update.as_view()),
+    url(r'^delete$', blog_delete.as_view()),
 ]
--- src/blogs/views.py
+++ src/blogs/views.py
@@ -4,18 +4,22 @@ from django.views import View
 
 # Create your views here.
 
-class blog_creat(View):
+class blog_create(View):
     def get(self, *args, **kwargs):
-        return HttpResponse("<h1>Hello</h1>")
+        return HttpResponse("<h1>Hello1</h1>")
 
 class blog_read(View):
     def get(self, *args, **kwargs):
-        return HttpResponse("<h1>Hello</h1>")
+        return HttpResponse("<h1>Hello2</h1>")
+
+class blog_list(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse("<h1>Hello3</h1>")
 
 class blog_update(View):
     def get(self, *args, **kwargs):
-        return HttpResponse("<h1>Hello</h1>")
+        return HttpResponse("<h1>Hello4</h1>")
 
 class blog_delete(View):
     def get(self, *args, **kwargs):
-        return HttpResponse("<h1>Hello</h1>")
+        return HttpResponse("<h1>Hello5</h1>")
